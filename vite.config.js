0215fd917f655012e49a02e0e434c7fa0942a964
zaidfadel1989 <zaid.fadel89@gmail.com>
2025-12-24 19:50:17 -0600

    Deploying 2

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/BookShow/' // Replace spaces with hyphens
+  base: '/BookShow/'  // This is correct since your folder is BookShow
 })
